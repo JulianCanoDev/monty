36a94c29b3662a14d078a7ee554209374dbdf879
julian <juliancano.dev@gmail.com>
2020-05-14 10:58:11 -0500

    main

--- monty.h
+++ monty.h
@@ -58,7 +58,6 @@ typedef struct instruction_s
 } instruction_t;
 
 void get_op(char *op, stack_t **stack, unsigned int line_number);
-static int check_for_digit(char *arg);
 void m_push(stack_t **stack, unsigned int line_number);
 void m_pall(stack_t **stack, unsigned int line_number);
 void m_pint(stack_t **stack, unsigned int line_number);
