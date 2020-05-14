cabd7cf332f5ca5d47f242ccad2411136f276ae3
julian <juliancano.dev@gmail.com>
2020-05-14 11:56:54 -0500

    testing

--- monty.h
+++ monty.h
@@ -38,9 +38,9 @@ extern var_t var;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -53,8 +53,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 void get_op(char *op, stack_t **stack, unsigned int line_number);
